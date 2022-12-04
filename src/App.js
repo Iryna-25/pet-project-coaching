49d9d52f873a0de71c0d97fec195b428c537be4d
Iryna Shandar <iryna.shandar@gmail.com>
2022-12-04 16:55:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default function App() {
   return (
     <Container>
       <Routes>
-        <Route path="*" 
+        <Route path="/" 
           element={ 
               <>
                 <SignUpPage/>
